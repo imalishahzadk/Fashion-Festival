83334d65cf5669478165209bad6afe6c67474dbc
HS180617 <150110363+HS180617@users.noreply.github.com>
2024-08-25 14:52:24 +0530

    corse added

--- server.js
+++ server.js
@@ -2,22 +2,28 @@ import Express from "express";
 import dbConnection from "./dataBase/dbConnection.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
-import Morgan from "morgan";
+import cors from "cors"; // Import cors
 import { init } from "./src/modules/index.routes.js";
 dotenv.config();
 
 const app = Express();
 dbConnection;
+
+// Enable CORS for all routes
+app.use(cors());
+
 app.use(Express.json());
 app.use(morgan("combined"));
 app.use(Express.static("uploads"));
-app.use(Morgan("dev"));
+app.use(morgan("dev"));
 init(app);
 
+// Error handling middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({ err: err.message, statusCode });
 });
-app.listen(3101, () => {
+
+app.listen(3100, () => {
   console.log("server is running");
 });
